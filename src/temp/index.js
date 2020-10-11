aebf55201033818bc73d3e8125193d0307984991
Chayadon <68224884+Chayadon@users.noreply.github.com>
2020-10-11 17:48:10 +0700

    Update index.js

--- src/temp/index.js
+++ src/temp/index.js
@@ -15,8 +15,8 @@ export default class Temp extends Component {
     return (
       <div align="center">
 
-	 <IframeComponent src="https://datastudio.google.com/embed/reporting/fbee6a90-980b-4bd5-b99a-dedb9111bfa8/page/qhijB" height="900" width="1300"/>
-	 <IframeComponent src="https://datastudio.google.com/embed/reporting/0fa05caa-db85-45b1-a2f2-c6a8afab42a2/page/CIijB" height="900" width="1300"/>
+	 <IframeComponent src="***********"/>
+	 <IframeComponent src="***********"/>
       </div>
     );
   }
