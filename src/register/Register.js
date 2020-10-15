d7b8f56426f71a41de18ea609ecc229a849ba750
Chayadon <68224884+Chayadon@users.noreply.github.com>
2020-10-15 19:06:29 +0700

    Update Register.js

--- src/register/Register.js
+++ src/register/Register.js
@@ -33,7 +33,7 @@ export default class Register extends Component{
     }
 
     handleClicked(){
-        let url = '******';
+        let url = '####/data';
         let data = {
             idkey:this.state.idkey,
             firstname:this.state.firstname,
